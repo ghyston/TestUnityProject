990169f36831d84451c2c6e49f9c8db06e32b300
ghyston <Hyston@Stepanovs-Air.fritz.box>
2016-02-05 00:33:02 +0100

    make bullets

--- Assets/MyScripts/CoolGuyController.cs
+++ Assets/MyScripts/CoolGuyController.cs
@@ -5,7 +5,11 @@ using UnityStandardAssets.CrossPlatformInput;
 public class CoolGuyController : MonoBehaviour {
 
 	Rigidbody2D myBody;
+	System.DateTime lastShot;
+
 	public float moveForce = 5;
+	public double timeBetweenBullets = 0.5d;
+	public GameObject bulletType;
 
 	// Use this for initialization
 	void Start () 
@@ -23,12 +27,23 @@ public class CoolGuyController : MonoBehaviour {
 
 		Vector2 angleVec = new Vector2 (CrossPlatformInputManager.GetAxis ("Horizontal1"), CrossPlatformInputManager.GetAxis ("Vertical1"));
 		float mg = angleVec.magnitude;
-		Debug.Log(string.Format("Magnitude: {0}", mg)); // mg 0..1
+		//Debug.Log(string.Format("Magnitude: {0}", mg)); // mg 0..1
 
 		float angle = Vector3.Angle(angleVec, new Vector3(1, 0, 0));
 		if (angleVec.y < 0) angle *= -1;
 		var rotationVector = transform.rotation.eulerAngles;
 		rotationVector.z = angle;
 		transform.rotation = Quaternion.Euler(rotationVector);
+
+		if (mg > 0.99) 
+		{
+			System.TimeSpan diff = System.DateTime.Now.Subtract (lastShot);
+			double totaSeconds = diff.TotalSeconds;
+			if (totaSeconds > timeBetweenBullets) 
+			{				
+				lastShot = System.DateTime.Now;
+				GameObject bullet = Instantiate (bulletType, transform.position, transform.rotation) as GameObject;
+			}
+		}
 	}
 }
---
+++ Assets/bulletScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletScript : MonoBehaviour {
+
+	public int speed = 6;
+
+	// Use this for initialization
+	void Start () {
+		Vector2 rotationEuler = transform.rotation * (new Vector2(1, 0));
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (rotationEuler.x, rotationEuler.y) * speed;
+		//Debug.Log ("rotation: " + rotationEuler.x + " " + rotationEuler.y);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void onBecameInvisible()
+	{
+		Destroy (gameObject);
+	}
+}
